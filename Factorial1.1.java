c83383303e8e255a72b5cd9aeda004c516a49e01
Nikolayzh <nikolay.zhivoderov@gmail.com>
2014-06-01 09:40:21 +0400

    Вычисление факториала

---
+++ Factorial1.1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NIK
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */  
+    public static void main(String[] args) {
+        // TODO code application logic here
+      int n;
+        do {
+            Scanner scan = new Scanner(System.in);
+             System.out.print("Введите факториал натурально числа n : ");
+              n = scan.nextInt();
+               int result = 1;
+                while( n> 0){
+                  result *= n--;}
+                  System.out.println(result);
+                if (n == 0 || n == 1)
+                 System.out.println(result);
+             if(n < 0)
+             System.out.println("Вы ввели отрицательный факториал!");
+        } while (n < 0);{System.out.println("Конец");}
+    }
+}
+    
+
